b7568eabbd3088836c8d1ae20e33751392fac241
Thomas Lehmann <thomas.lehmann@stueckseln.de>
2018-02-04 17:29:09 +0100

    Extended example to import path-relative module

--- js/requirejs/example-project/src/foo/main.js
+++ js/requirejs/example-project/src/foo/main.js
@@ -2,14 +2,17 @@ require([
 	"./config",
 	"component",
 	"component/util/some-util",
+	"somelib/complex/foocomplex",
 	"meh"
 ], function(
 	_config,
 	Component,
 	SomeUtil,
+	FooComplex,
 	meh
 ) {
 	console.log("main loaded component: ", Component);
 	console.log("component-relative: ", SomeUtil);
+	console.log("somelib-relative: ", FooComplex);
 	console.log("meh: ", meh);
 });
