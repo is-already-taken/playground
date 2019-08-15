bcd151a188fafe1c0a34f4496ddb9f07732a4568
Thomas Lehmann <thomas.lehmann@stueckseln.de>
2019-08-15 21:38:16 +0200

    largest contiguous area: Add skeleton and problem description

---
+++ by-problem/graphics/largest-area/solution.js
@@ -0,0 +1,14 @@
+
+/* Find the largest contiguous area.
+ * Return the input matrix with the area's values replaced by "X".
+ * Don't care for performance or memory efficiency _in the first place_,
+ * get the algorithm done.
+ * Further constraints:
+ * - Assume there are no extremes like 1xN or Nx1
+ */
+
+function findContiguousArea(data) {
+	return [];
+}
+
+export { findContiguousArea };
