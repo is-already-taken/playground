3151ed7fd1b804b9f79233e92b17fd9c2b9c481b
Thomas Lehmann <thomas.lehmann@stueckseln.de>
2019-11-17 19:01:16 +0100

    opencv/Set game - Add first basic color detection

---
+++ by-problem/opencv/set-game/src/main/java/net/acme/opencv/setgame/ColorDetection.java
@@ -0,0 +1,59 @@
+package net.acme.opencv.setgame;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+
+import net.acme.opencv.setgame.utils.Histogram;
+
+/**
+ * Detect fill of a shape
+ */
+public class ColorDetection {
+
+	/**
+	 * Process card image to get color.
+	 * 
+	 * @param image color image of a shape
+	 * @param location location of a shape on the image
+	 * @return color
+	 */
+	public static Color process(Mat image, Rect location) {
+		Histogram blueHistogram = Histogram.generate(image, 0);
+		Histogram greenHistogram = Histogram.generate(image, 1);
+		Histogram redHistogram = Histogram.generate(image, 2);
+
+		// Pair color-index with the corresponding average value
+		int[][] rgb = new int[][] {
+				{ 0, redHistogram.average() },
+				{ 1, greenHistogram.average() },
+				{ 2, blueHistogram.average() }
+		};
+
+		int colorIndex = Arrays.asList(rgb)
+			.stream()
+			.max(new RgbValuePeakComparator())
+			.get()[0];
+
+		return Color.values()[colorIndex];
+	}
+
+	/**
+	 * The color
+	 */
+	public static enum Color {
+		red, green, blue
+	}
+
+	/**
+	 * Compares two color-index/value tuples for their value.
+	 */
+	private static class RgbValuePeakComparator implements Comparator<int[]> {
+		@Override
+		public int compare(int[] o1, int[] o2) {
+			return (o1[1] < o2[1]) ? -1 : (o1[1] > o2[1] ? 1 : 0);
+		}
+	}
+}
