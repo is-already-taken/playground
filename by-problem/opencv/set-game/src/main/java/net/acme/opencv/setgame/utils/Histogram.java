4ebff357f567bbe031011e9eff57bcee8aa85de5
Thomas Lehmann <thomas.lehmann@stueckseln.de>
2019-11-18 22:12:57 +0100

    opencv/Set game - Histogram provides modes()

--- by-problem/opencv/set-game/src/main/java/net/acme/opencv/setgame/utils/Histogram.java
+++ by-problem/opencv/set-game/src/main/java/net/acme/opencv/setgame/utils/Histogram.java
@@ -2,7 +2,13 @@ package net.acme.opencv.setgame.utils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
@@ -57,6 +63,57 @@ public class Histogram {
 	}
 
 	/**
+	 * Get the modes of the histogram.
+	 * 
+	 * Its not guaranteed that the resulting list contains up to the number of
+	 * modes specified.
+	 * 
+	 * @param minValueDistance accept only values with a minimum distance to the
+	 *        last mode
+	 * @param grades number of modes
+	 * @return list of modes with most frequent value first.
+	 */
+	public List<Integer> modes(int minValueDistance, int grades) {
+		List<Integer> normalizedValues = normalized();
+		List<Integer> modes = new ArrayList<>();
+		final AtomicInteger index = new AtomicInteger(0);
+		int grade = 0;
+		int lastValue;
+
+		if (grades <= 0 || grades >= BIN_COUNT) {
+			throw new IllegalArgumentException("Number of grades must be between 1 - " + BIN_COUNT);
+		}
+
+		// Pair values with their frequency (count) and sort by the frequency.
+		// Map to value, most frequent value last.
+		normalizedValues = normalizedValues.stream()
+			.map((value) -> new int[] { index.getAndIncrement(), value })
+			.sorted(new CompareByFrequency())
+			.map((valueFrequencyPair) -> valueFrequencyPair[0])
+			.collect(Collectors.toList());
+
+		// Most frequent value first
+		Collections.reverse(normalizedValues);
+
+		// First mode
+		modes.add(normalizedValues.get(0));
+		lastValue = normalizedValues.get(0);
+
+		while (modes.size() < grades || grade >= MAX_VALUE) {
+			int value = normalizedValues.get(grade);
+
+			if (lastValue - value > minValueDistance) {
+				modes.add(value);
+				lastValue = value;
+			}
+
+			grade++;
+		}
+
+		return modes;
+	}
+
+	/**
 	 * Get the histogram with raw counts.
 	 * 
 	 * @return binned counts per value
@@ -93,5 +150,20 @@ public class Histogram {
 			MAX_VALUE), new MatOfFloat(0, BIN_COUNT), true);
 
 		return new Histogram(histogram, image.rows() * image.cols());
+	}
+
+	/**
+	 * Compare a pair of value-frequency by the frequency.
+	 */
+	private static class CompareByFrequency implements Comparator<int[]> {
+
+		@Override
+		public int compare(int[] o1, int[] o2) {
+			int freq1 = o1[1];
+			int freq2 = o2[1];
+
+			return (freq1 < freq2) ? -1 : (freq1 > freq2 ? 1 : 0);
+		}
+
 	}
 }
