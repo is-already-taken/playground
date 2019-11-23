e55ad3d270010139747d1ba5303575ba13c19b82
Thomas Lehmann <thomas.lehmann@stueckseln.de>
2019-11-23 14:23:57 +0100

    opencv/Set game - Color detection: check histogram over smaller sub image
    In order to reduce the contribution of the white area surrounding the shape
    swallowing the actual color values (kind of SNR), a smaller portion is probed.

--- by-problem/opencv/set-game/src/main/java/net/acme/opencv/setgame/ColorDetection.java
+++ by-problem/opencv/set-game/src/main/java/net/acme/opencv/setgame/ColorDetection.java
@@ -5,6 +5,8 @@ import java.util.Comparator;
 
 import org.opencv.core.Mat;
 import org.opencv.core.Rect;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 
 import net.acme.opencv.setgame.utils.Histogram;
 
@@ -12,20 +14,36 @@ import net.acme.opencv.setgame.utils.Histogram;
  * Detect fill of a shape
  */
 public class ColorDetection {
+	// Pick tiny probe of the shape center to limit 
+	// artifacts/disturbances caused by the border.
+	private static final int PROBE_SIZE = 4;
+
+	// Blur image to get average value of the shape content
+	private static final Size BLUR_KERNEL = new Size(new double[] { 4, 4 });
 
 	/**
 	 * Process card image to get color.
 	 * 
 	 * @param image color image of a shape
 	 * @param location location of a shape on the image
+	 * @param string
 	 * @return color
 	 */
-	public static Color process(Mat image, Rect location) {
-		Mat shapeImage = image.submat(location);
+	public static Color process(Mat image, Rect location, String string) {
+		// Vertically centered slice to have as little white space as possible 
+		Rect probe = new Rect(
+			location.x,
+			location.y + Math.round(location.height / 2) - (PROBE_SIZE / 2),
+			Math.round(location.width / 2) + 5,
+			PROBE_SIZE + 5);
+		Mat probeImage = image.submat(probe);
+		Mat blurred = new Mat();
 
-		Histogram blueHistogram = Histogram.generate(shapeImage, 0);
-		Histogram greenHistogram = Histogram.generate(shapeImage, 1);
-		Histogram redHistogram = Histogram.generate(shapeImage, 2);
+		Imgproc.blur(probeImage, blurred, BLUR_KERNEL);
+
+		Histogram blueHistogram = Histogram.generate(blurred, 0);
+		Histogram greenHistogram = Histogram.generate(blurred, 1);
+		Histogram redHistogram = Histogram.generate(blurred, 2);
 
 		// Pair color-index with the corresponding average value
 		int[][] rgb = new int[][] {
